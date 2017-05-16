811bb2c1b3173ca748205f790f66ca6777db467b
denis <denis375445382598@gmail.com>
2017-05-16 23:36:35 +0300

    some test

--- src/test/opinionpull/dao/factory/DAOFactoryTest.java
+++ src/test/opinionpull/dao/factory/DAOFactoryTest.java
@@ -1,41 +1,58 @@
 package test.opinionpull.dao.factory;
 
+import by.tc.opinionpull.dao.*;
+import by.tc.opinionpull.dao.factory.DAOFactory;
+import by.tc.opinionpull.dao.impl.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DAOFactoryTest {
 	@Test
 	public void getInstance() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		Assert.assertTrue(daoFactory instanceof DAOFactory);
 	}
 
 	@Test
 	public void getAnswerDAO() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		AnswerDAO answerDAO = daoFactory.getAnswerDAO();
+		Assert.assertTrue(answerDAO instanceof AnswerDAOImpl);
 	}
 
 	@Test
 	public void getPollDAO() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		PollDAO pollDAO = daoFactory.getPollDAO();
+		Assert.assertTrue(pollDAO instanceof PollDAOImpl);
 	}
 
 	@Test
 	public void getQuestionDAO() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		QuestionDAO questionDAO = daoFactory.getQuestionDAO();
+		Assert.assertTrue(questionDAO instanceof QuestionDAOImpl);
 	}
 
 	@Test
 	public void getTestDAO() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		TestDAO testDAO = daoFactory.getTestDAO();
+		Assert.assertTrue(testDAO instanceof TestDAOImpl);
 	}
 
 	@Test
 	public void getTopicDAO() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		TopicDAO topicDAO = daoFactory.getTopicDAO();
+		Assert.assertTrue(topicDAO instanceof TopicDAOImpl);
 	}
 
 	@Test
 	public void getUserDAO() throws Exception {
-		//TODO
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		UserDAO userDAO = daoFactory.getUserDAO();
+		Assert.assertTrue(userDAO instanceof UserDAOImpl);
 	}
 
 }
--- src/test/opinionpull/service/factory/ServiceFactoryTest.java
+++ src/test/opinionpull/service/factory/ServiceFactoryTest.java
@@ -1,41 +1,57 @@
 package test.opinionpull.service.factory;
 
+import by.tc.opinionpull.service.*;
+import by.tc.opinionpull.service.factory.ServiceFactory;
+import by.tc.opinionpull.service.impl.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServiceFactoryTest {
 	@Test
 	public void getInstance() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+		Assert.assertTrue(serviceFactory instanceof ServiceFactory);
 	}
 
 	@Test
 	public void getAnswerService() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+		AnswerService answerService = serviceFactory.getAnswerService();
+		Assert.assertTrue(answerService instanceof AnswerServiceImpl);
 	}
 
 	@Test
 	public void getPollService() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+		PollService pollService = serviceFactory.getPollService();
+		Assert.assertTrue(pollService instanceof PollServiceImpl);
 	}
 
 	@Test
 	public void getQuestionService() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+		QuestionService questionService = serviceFactory.getQuestionService();
+		Assert.assertTrue(questionService instanceof QuestionServiceImpl);
 	}
 
 	@Test
 	public void getTestService() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+
 	}
 
 	@Test
 	public void getTopicService() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+		TopicService topicService = serviceFactory.getTopicService();
+		Assert.assertTrue(topicService instanceof TopicServiceImpl);
 	}
 
 	@Test
 	public void getUserService() throws Exception {
-		//TODO
+		ServiceFactory serviceFactory = ServiceFactory.getInstance();
+		UserService userService = serviceFactory.getUserService();
+		Assert.assertTrue(userService instanceof UserServiceImpl);
 	}
 
 }
